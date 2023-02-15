fe4d89a86e92ab4b7d35a0b3479ac12fb56d4466
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-02-15 23:34:35 +0530

    added get data route and home routes

--- blog-express/routes/Home.js
+++ blog-express/routes/Home.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import { HomeGet } from '../controllers/HomeController.js'
+import { getBlog, getData } from "../controllers/BlogController.js";
 const router2 = Router();
 
 router2.get('/', HomeGet)
+
+router2.post("/blog", getBlog)
+router2.get("/getdata", getData)
 
 export default router2;
