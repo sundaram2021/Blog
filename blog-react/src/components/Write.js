9186de2450aa854a1a0672021550596c6e1a1078
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-02-16 18:13:24 +0530

    modified

--- blog-react/src/components/Write.js
+++ blog-react/src/components/Write.js
@@ -1,45 +1,48 @@
-import React, { useState, useEffect } from "react";
-import { useQuill } from 'react-quilljs';
-import 'quill/dist/quill.snow.css';
+import React, { useState } from "react";
+import { useQuill } from "react-quilljs";
+import "quill/dist/quill.snow.css";
 import Navbar from "./Navbar";
 import "../css/write.css";
 import { useNavigate } from "react-router-dom";
+import 'react-quill/dist/quill.bubble.css'
 
 function Write() {
   const navigate = useNavigate();
   const [value, setValue] = useState({
     title: "",
-    body: ""
-  })
+    body: "",
+  });
 
   const { quill, quillRef } = useQuill({
-    modules : {
+    modules: {
       toolbar: [
-        ['bold', 'italic', 'underline', 'strike'],
-        [{ 'header': 1 }, { 'header': 2 }],
-        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-        [{ 'script': 'sub'}, { 'script': 'super' }],
-        [{ 'indent': '-1'}, { 'indent': '+1' }],
-        [{ 'direction': 'rtl' }],
-        [{ 'size': ['small', false, 'large', 'huge'] }],
-        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-        [{ 'color': [] }, { 'background': [] }],
-        [{ 'font': [] }],
-        [{ 'align': [] }],
-        ['clean'],
-        ['link', 'image', 'video']
-      ]
-    }
+        ["bold", "italic", "underline", "strike"],
+        [{ header: 1 }, { header: 2 }],
+        [{ list: "ordered" }, { list: "bullet" }],
+        [{ script: "sub" }, { script: "super" }],
+        [{ indent: "-1" }, { indent: "+1" }],
+        [{ direction: "rtl" }],
+        [{ size: ["small", false, "large", "huge"] }],
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        [{ color: [] }, { background: [] }],
+        [{ font: [] }],
+        [{ align: [] }],
+        ["clean"],
+        ["link", "image", "video"],
+      ],
+    },
   });
 
   if (quill) {
-    quill.on('text-change', () => {
-      setValue({...value, body: quillRef.current.firstChild.innerHTML}); // Get innerHTML using quillRef
+    quill.on("text-change", () => {
+      // console.log("Text = "+quillRef.current.firstChild.getText());
+      // const plainText = quillRef.current.getText();
+      // console.log(plainText);
+      setValue({ ...value, body: removeHtmlTags(quillRef.current.firstChild.innerHTML) }); // Get innerHTML using quillRef
     });
   }
 
-
-
+  console.log(value);
 
   async function handleBlog() {
     console.log(value);
@@ -48,15 +51,20 @@ function Write() {
       method: "POST",
       body: JSON.stringify(value),
       headers: {
-        'Content-Type': "application/json",
-      }
-    })
-    if(res.ok){
-      setValue("")
-      alert('Blog is Posted');
+        "Content-Type": "application/json",
+      },
+    });
+    if (res.ok) {
+      setValue("");
+      alert("Blog is Posted");
       navigate("/");
     }
   }
+
+  function removeHtmlTags(text) {
+    return text.replace(/<[^>]+>/g, '');
+  }
+
   return (
     <>
       <Navbar />
@@ -66,14 +74,26 @@ function Write() {
             value={value.title}
             type="text"
             placeholder="enter the title..."
-            onChange={(e) => setValue({...value, title: e.target.value})}
+            onChange={(e) => setValue({ ...value, title: e.target.value })}
           />
           <div className="icons-div">
             <div>
-              <div ref={quillRef}  style={{color: 'black', fontSize: '1.1rem', height: '35vh', border: 'none'}} />
+              <div
+                ref={quillRef}
+                style={{
+                  color: "black",
+                  fontSize: "1.1rem",
+                  height: "35vh",
+                  border: "none",
+                }}
+                theme={"bubble"}
+                
+              />
             </div>
-          </div>  
-          <button className="button" onClick={handleBlog}>publish</button>
+          </div>
+          <button className="button" onClick={handleBlog}>
+            publish
+          </button>
         </div>
       </div>
     </>
