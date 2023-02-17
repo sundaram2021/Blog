670b0ec49abb97a21bc03ba72551a76f8bc4d104
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-02-18 00:31:30 +0530

    modified

--- blog-react/src/pages/Home .js
+++ blog-react/src/pages/Home .js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import "../css/home.css";
 import img2 from "../images/write.png";
