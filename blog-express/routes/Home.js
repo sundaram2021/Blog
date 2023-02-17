ba412a4ebd43e2aa8ee9167f375df2bf37d7954d
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-02-18 00:31:58 +0530

    added blog route

--- blog-express/routes/Home.js
+++ blog-express/routes/Home.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { HomeGet } from '../controllers/HomeController.js'
-import { getBlog, getData } from "../controllers/BlogController.js";
+import { getBlog, getData, getBlogData } from "../controllers/BlogController.js";
 const router2 = Router();
 
 router2.get('/', HomeGet)
 
 router2.post("/blog", getBlog)
 router2.get("/getdata", getData)
+router2.get("/getdata/:id", getBlogData)
 
 export default router2;
