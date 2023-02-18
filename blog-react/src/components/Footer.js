d398e87ea8ba30662df30c5302917920e66c50fc
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-02-18 18:29:20 +0530

    added icons and updated some css

--- blog-react/src/components/Footer.js
+++ blog-react/src/components/Footer.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import "../css/home.css";
-import SearchBar from "./SearchBar";
 
 function Footer() {
   return (
     <footer className='footer'>
       <h4>Hello footer</h4>
-      <div>
-          <SearchBar />
-        </div>
     </footer>
   )
 }
