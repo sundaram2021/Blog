ebdc01c953392f2f72285b593a56e1d978ca2063
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-03-03 23:06:47 +0530

    added like and save entities

--- blog-express/Models/BlogModel.js
+++ blog-express/Models/BlogModel.js
@@ -5,6 +5,14 @@ const { Schema } = mongoose
 const BlogSchema = new Schema({
     title: String,
     body: String,
+    like: {
+        type: Boolean, 
+        default: false,
+    },
+    save: {
+        type: Boolean, 
+        default: false,
+    },
 })
 
 export default new mongoose.model("Blogs", BlogSchema);
