88a225d60b7b977487a73188decb08ac6975f7c7
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-03-04 22:12:58 +0530

    added routes for comment

--- blog-express/routes/Home.js
+++ blog-express/routes/Home.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { HomeGet } from '../controllers/HomeController.js'
-import { getBlog, getData, getBlogData, postLikeData, postSaveData } from "../controllers/BlogController.js";
+import { getBlog, getData, getBlogData, postLikeData, postSaveData, postComment } from "../controllers/BlogController.js";
 const router2 = Router();
 
 router2.get('/', HomeGet)
@@ -8,7 +8,8 @@ router2.get('/', HomeGet)
 router2.post("/blog", getBlog)
 router2.get("/getdata", getData)
 router2.get("/getdata/:id", getBlogData)
-router2.post("postlike", postLikeData)
-router2.post("postsave", postSaveData)
+router2.post("/postlike", postLikeData)
+router2.post("/postsave", postSaveData)
+router2.post("/postcomment", postComment)
 
 export default router2;
