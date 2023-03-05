52950b06b3da99fa7c269cee1cecada7f7f5e7b8
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-03-05 19:25:28 +0530

    added comments

--- blog-react/src/components/Blogs.js
+++ blog-react/src/components/Blogs.js
@@ -8,11 +8,12 @@ import { FaRegBookmark } from "react-icons/fa";
 import { FaShareAlt } from "react-icons/fa";
 import { FaBookmark } from "react-icons/fa";
 import { v4 as uuidv4 } from "uuid";
+import UserImage from "./UserImage";
 
 function Blogs() {
+  let c;
   const [blogs, setBlogs] = useState([]);
-  const [comment, setComment] = useState(false);
-  const [commentValue, setCommentValue] = useState();
+  // const [commentValue, setCommentValue] = useState();
 
   const handleClick = (id) => {
     console.log(id);
@@ -40,7 +41,7 @@ function Blogs() {
     });
     if (res.ok) {
       const { myBlogs } = await res.json();
-      console.log(myBlogs);
+      // console.log(myBlogs);
       // console.log("myblogs "+myBlogs);
       setBlogs(myBlogs);
     }
@@ -55,8 +56,8 @@ function Blogs() {
 
     const obj = { _id: id };
     const json = JSON.stringify(obj);
-    console.log("liked");
-    console.log("json = " + json);
+    // console.log("liked");
+    // console.log("json = " + json);
 
     const res = await fetch("http://localhost:8999/postlike", {
       method: "POST",
@@ -99,7 +100,7 @@ function Blogs() {
 
   const publishComment = async (e, id) => {
     e.preventDefault();
-    const obj = { _id: id, comment: commentValue };
+    const obj = { _id: id, comment: c };
     const json = JSON.stringify(obj);
     const res = await fetch("http://localhost:8999/postcomment", {
       method: "POST",
@@ -115,43 +116,50 @@ function Blogs() {
     }
   };
 
-  const handleComment = async(idx) => {
-    setComment(!comment);
+  const handleComment = async (idx) => {
+    // setComment(!comment);
     const obj = { _id: idx };
     const json = JSON.stringify(obj);
 
     const res = await fetch("http://localhost:8999/postcomment", {
       method: "POST",
       body: json,
-      headers : {
+      headers: {
         "Content-Type": "application/json",
       },
-    })
+    });
 
     if (res.ok) {
       getData();
       // alert(message)
     }
-    
   };
 
-  const closeComment = async(idx) => {
-    setComment(!comment);
+  const closeComment = async (idx) => {
     const obj = { _id: idx };
     const json = JSON.stringify(obj);
 
     const res = await fetch("http://localhost:8999/postcomment", {
+      method: "POST",
       body: json,
-      headers : {
+      headers: {
         "Content-Type": "application/json",
       },
-    })
+    });
 
     if (res.ok) {
       getData();
       // alert(message)
     }
-    
+  };
+
+  const handleCommentChange = (e, idx) => {
+    e.preventDefault();
+    // console.log(e.target.value);
+    // console.log(idx);
+    // setCommentValue(e.target.value);
+
+    c = e.target.value;
   };
 
   return (
@@ -186,13 +194,34 @@ function Blogs() {
                   <span onClick={() => closeComment(item._id)}>x</span>
                   <input
                     type="text"
-                    onChange={(e) => setCommentValue(e.target.value)}
+                    onChange={(e) => handleCommentChange(e, item._id)}
                   />
                   <button onClick={(e) => publishComment(e, item._id)}>
                     publish
                   </button>
                 </section>
               )}
+              
+              {item.comment.map((cmt) => {
+                if (!cmt.text || cmt.text.trim() === "") {
+                  return null;
+                }
+                return (
+                  <section className="comment-section" key={cmt.createdAt}>
+                    <h3>Comments...</h3>
+                    <div className="comment-section-div">
+                      <UserImage
+                        className="comment-section-img"
+                        title={cmt.author}
+                      />
+                      <div className="comment-section-nested-div">
+                        <p className="date">published at {cmt.createdAt}</p>
+                        <p className="actual-comment">{cmt.text}</p>
+                      </div>
+                    </div>
+                  </section>
+                );
+              })}
             </div>
           </main>
         );
