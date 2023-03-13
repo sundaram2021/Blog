93c3fc46be57b59cdaff2d5b241e71e44fa9c437
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-03-13 23:51:02 +0530

    getting info from server

--- blog-react/src/components/UserImage.js
+++ blog-react/src/components/UserImage.js
@@ -1,20 +1,27 @@
 import React from "react";
 import "../css/Avatar.css";
 import img from "../images/img.jpg";
-const UserImage = () => {
-  const name = localStorage.getItem("firstName");
-  const initials = name[0].toUpperCase();
-  const url = `https://dummyimage.com/40x50/999/fff&text=${initials}`;
+const UserImage = ({userName}) => {
+  
+
+  // const name = localStorage.getItem("firstName");
+  // const initials =  userName[0].toUpperCase() : " ";
+  // const url = `https://dummyimage.com/40x50/999/fff&text=${initials}`;
 
   const styles = {
     color: "black",
     fontSize: "10px",
     fontWeight: "800",
-    backgroundColor: "#" + Math.floor(100000 + Math.random() * 900000),
+    // backgroundColor: "#" + Math.floor(100000 + Math.random() * 900000),
   };
 
+  // const firstName = "John";
+  const firstInitial = userName ? userName.charAt(0).toUpperCase() : "";
+  const randomHexColor = Math.floor(Math.random() * 16777215).toString(16); // generates a random hexadecimal color code
+  const url = `https://dummyimage.com/40x50/${randomHexColor}/fff.png&text=${firstInitial}`;
+
   return (
-    <img className="user-image" style={styles} src={url !== "" ? url : img} />
+    <img className="user-image" style={styles} src={url !== "" ? url : img} alt='' />
   );
 };
 
