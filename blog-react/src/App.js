a3c8a75cd21d0a675057798d571ca30783a64c9a
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-03-14 22:30:34 +0530

    modified

--- blog-react/src/App.js
+++ blog-react/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useEffect, useCallback, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home ";
 import Blogs from "./components/Blogs";
@@ -9,8 +10,28 @@ import Blog from "./components/Blog";
 import Saved from "./components/Saved";
 
 function App() {
+  const [userName, setUserName] = useState();
+  const getUser = useCallback(async (token) => {
+    const res = await fetch("http://localhost:8999", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "x-access-token": token,
+      },
+    });
+  
+    if (res.ok) {
+      const { firstName } = await res.json();
+      await setUserName(firstName);
+      // localStorage.setItem("firstName", firstName);
+    }
+  }, []);
+  
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem("token2")); // set your token here
+    getUser(token);
+  }, [getUser]);
 
-  const username = localStorage.getItem("firstName")
 
   return (
     <>
@@ -21,7 +42,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/blogs/:id" element={<Blog />} />
-        <Route path={`/${username}/savedArticles`} element={<Saved />} />
+        <Route path={`/${userName}/savedArticles`} element={<Saved />} />
       </Routes>
     </>
   );
